3662442e02539bfa3225036d86bd5926c4f22549
Andria04 <ericka.andria04@gmail.com>
2023-06-19 15:28:03 -0700

    combinations

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints all combinations of single digits
+ * Return: void
+ */
+
+int main(void)
+{
+	char separator = ',';
+	char number = '0';
+
+	while (number <= '9')
+	{
+		if (number != '0')
+		{
+			putchar((int)separator);
+			putchar(' ');
+		}
+		putchar((int)number);
+		number++;
+	}
+	putchar('\n');
+	return (0);
+}
