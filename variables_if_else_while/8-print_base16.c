b42a00f0fb47f89b108b6b8e9c834348dedc2975
Andria04 <ericka.andria04@gmail.com>
2023-06-19 09:46:57 -0700

    base 16

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints base 16
+ * Return: void
+ */
+
+int main(void)
+{
+	char number = '0';
+	char letter = 'a';
+
+	while (number <= '9')
+	{
+		putchar(number);
+		number++;
+	}
+	while (letter <= 'f')
+	{
+		putchar(letter);
+		letter++;
+	}
+	putchar('\n');
+	return(0);
+}
