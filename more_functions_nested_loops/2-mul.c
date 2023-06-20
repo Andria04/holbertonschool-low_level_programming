d71579d0eebf248c2d62a57f261d9cff8f22ed2e
Andria04 <ericka.andria04@gmail.com>
2023-06-20 16:02:56 -0700

    multiply

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * mul - multiply two integers
+ * @a: the first integer
+ * @b: the second integer
+ * Return: 0
+ */
+
+int mul(int a, int b)
+{
+	int result = 0;
+
+	result = a * b;
+
+	return (result);
+}
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -4,5 +4,6 @@
 /* prototype goes here */
 int _isupper(int c);
 int _isdigit(int c);
+int mul(int a, int b);
 
 #endif /* MAIN_H */
