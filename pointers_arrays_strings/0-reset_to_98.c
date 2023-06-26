0d2f19218aec1a9127eb9959dd551e61579b1df1
Andria04 <ericka.andria04@gmail.com>
2023-06-25 23:23:15 -0700

    reset pointers

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * reset_to_98 - updates the value to 98
+ * @n: the parameter
+ * Return: always 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ pointers_arrays_strings/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Prorotype goes here */
+
+void reset_to_98(int *n);
+
+#endif /* MAIN_H */
