8ec282a0caff7e71350d04c920cb52413bc124cc
Andria04 <ericka.andria04@gmail.com>
2023-06-27 01:28:09 -0700

    concatenates

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * strcat - concatenates thes tring pointed to by £src
+ * @dest: a pointer to the string
+ * @src: the source string
+ * Return: a pointer to the dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int index = 0, dest_len = 0;
+
+	while (dest[index++])
+		dest_len++;
+
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
+
+	return (dest);
+}
