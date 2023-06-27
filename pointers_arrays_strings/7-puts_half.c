5dc0439c52ce99f3f6f256e9e70f14987fb0d4f8
Andria04 <ericka.andria04@gmail.com>
2023-06-27 00:59:33 -0700

    half

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -10,12 +10,18 @@
 
 void puts_half(char *str)
 {
-	int index = 0, len = 0;
+	int index = 0, len = 0, n;
 
 	while (str[index++])
 		len++;
 
-	for (index = 0; index < len; index += 2)
+	if ((len % 2) == 0)
+		n = len / 2;
+
+	else
+		n = (len + 1) / 2;
+
+	for (index = n; index < len; index++)
 		putchar(str[index]);
 
 	putchar('\n');
