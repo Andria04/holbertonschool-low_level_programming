5581c54974d51d861851a3bd74a905db1f6377a2
Andria04 <ericka.andria04@gmail.com>
2023-06-27 01:06:21 -0700

    copy

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strcpy - copies a string
+ * @dest: a buffer to copy the string to
+ * @src: the source string to copy
+ * Return: A pointer to the dest
+ */
+
+char *_strcpy(char *dest, const char *src)
+{
+	int index = 0;
+
+	while (src[index])
+	{
+		dest[index] = src[index];
+		index++;
+	}
+
+	return (dest);
+}
